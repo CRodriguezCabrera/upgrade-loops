e8d59f6216770591736dce8cd35d07f4b03f20df
ZEEKER <zeeker@MacBook-Air-de-ZEEKER.local>
2021-10-07 23:52:08 +0200

    upgrade-loops

---
+++ iteration1.js
@@ -0,0 +1,14 @@
+/* **Iteración #1: Usa includes**
+
+Haz un bucle y muestra por consola todos aquellos valores del array que incluyan la palabra "Camiseta".
+ Usa la función .***includes*** de javascript.
+
+ */
+const products = ['Camiseta de Pokemon', 'Pantalón coquinero', 'Gorra de gansta', 'Camiseta de Basket', 'Cinrurón de Orión', 'AC/DC Camiseta']
+
+for (let i = 0; i < products.length; i++) {
+    if(products[i].includes("camiseta")){
+        console.log(products[i])
+    }
+    
+}
